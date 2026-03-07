21ca23e54440dc2cf93f4b3cce5ac35d910997bf
YYUU1407 <136756836+YYUU1407@users.noreply.github.com>
2026-03-07 13:55:21 +0100

    Update player.py

--- player.py
+++ player.py
@@ -35,7 +35,7 @@ class TransformerPlayer(Player):
    def __init__(
         self,
         name: str,
-        model_name = "Qwen/Qwen3.5-397B-A17B",
+        model_name = "deepseek-ai/DeepSeek-V3.2",
         seed: int = 123,  #ensure repeatble results
         use_4bit: bool = True,  #use model in 4-bit to fit in Colab GPU memory
         # generation fallback params
@@ -265,9 +265,9 @@ bnb_config = BitsAndBytesConfig(
     bnb_4bit_compute_dtype=torch.float16,     #defining new bit config (4bit) for new syntax off model load
     bnb_4bit_use_double_quant=True
 )
-tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen3.5-397B-A17B")
+tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/DeepSeek-V3.2")
 model = AutoModelForCausalLM.from_pretrained(
-    "Qwen/Qwen3.5-397B-A17B",
+    "deepseek-ai/DeepSeek-V3.2",
     device_map="auto",
     quantization_config=bnb_config
     )
